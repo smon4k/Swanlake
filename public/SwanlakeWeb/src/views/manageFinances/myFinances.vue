cf1042742bd60856da55e000e5dfaedf74c5c64e
smon4k <smon4k@gmail.com>
2022-07-14 21:11:22 +0800

    no message

--- public/SwanlakeWeb/src/views/manageFinances/myFinances.vue
+++ public/SwanlakeWeb/src/views/manageFinances/myFinances.vue
@@ -97,7 +97,7 @@
                 <el-descriptions-item label="总结余(USDT)">{{ toFixed(item.total_balance || 0, 2) }}</el-descriptions-item>
                 <el-descriptions-item label="购买份数">{{ toFixed(item.total_number || 0, 2) }}</el-descriptions-item>
                 <!-- <el-descriptions-item label="购    买时间">{{ item.time }}</el-descriptions-item> -->
-                <el-descriptions-item label="净值">{{ toFixed(item.buy_networth || 0, 4) }}</el-descriptions-item>
+                <el-descriptions-item label="净值">{{ toFixed(item.networth || 0, 4) }}</el-descriptions-item>
                 <el-descriptions-item label="昨日收益(USDT)">{{ toFixed(item.yest_income || 0, 2) }}</el-descriptions-item>
                 <el-descriptions-item label="总收益率">{{ toFixed(item.total_rate || 0, 2) }}%</el-descriptions-item>
                 <el-descriptions-item label="年化收益率">{{ toFixed(item.year_rate || 0, 2) }}%</el-descriptions-item>
