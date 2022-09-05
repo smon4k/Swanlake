c8dc5e9950d3bba4b7457cdd95190c928f559019
smon4k <smon4k@gmail.com>
2022-09-05 10:13:48 +0800

    no message

--- public/AdminServer/src/pages/Piggybank/date.vue
+++ public/AdminServer/src/pages/Piggybank/date.vue
@@ -80,6 +80,11 @@
                                 <span>{{ keepDecimalNotRounding(scope.row.total_balance, 4, true) }} USDT</span>
                                 </template>
                             </el-table-column>
+                            <el-table-column prop="" label="币价" align="center">
+                                <template slot-scope="scope">
+                                <span>{{ keepDecimalNotRounding(scope.row.price, 4, true) }} USDT</span>
+                                </template>
+                            </el-table-column>
                             <el-table-column prop="" label="日利润" align="center">
                                 <template slot-scope="scope">
                                 <span>{{ keepDecimalNotRounding(scope.row.daily_profit, 4, true) }} USDT</span>
@@ -133,6 +138,11 @@
                             <el-table-column prop="" label="总结余" align="center">
                                 <template slot-scope="scope">
                                 <span>{{ keepDecimalNotRounding(scope.row.total_balance, 4, true) }} BTC</span>
+                                </template>
+                            </el-table-column>
+                            <el-table-column prop="" label="币价" align="center">
+                                <template slot-scope="scope">
+                                <span>{{ keepDecimalNotRounding(scope.row.price, 4, true) }} USDT</span>
                                 </template>
                             </el-table-column>
                             <el-table-column prop="" label="日利润" align="center">
