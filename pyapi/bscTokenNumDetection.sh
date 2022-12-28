72a342b232030e270b1a9a5749d770a473cf6996
smon4k <smon4k@gmail.com>
2022-12-28 19:05:54 +0800

    no message

---
+++ pyapi/bscTokenNumDetection.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "开始检查python脚本是否运行..."
+
+cd /home/www/Swanlake/pyapi/
+bscTokenNum=$(netstat -an|grep LISTEN | grep 8013)
+echo $bscTokenNum
+# 判断$bscTokenNum为空,此处意思为如果$bscTokenNum为空，那么重启
+if ["$bscTokenNum"!=""];
+then
+    # 启动python脚本
+    echo "python脚本[bsc币种新增地址量统计 端口8013]未运行, 重新启动中..."
+    nohup python3 ./bscTokenNum.py >/dev/null &
+    echo "python脚本[bsc币种新增地址量统计 端口8013]重启成功..."
+else
+    echo "python脚本[bsc币种新增地址量统计 端口8013]正在运行中..."
+fi
