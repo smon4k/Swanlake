6e82cd92590677443c146a89d70353ceeaf73d01
smon4k <smon4k@gmail.com>
2023-01-29 22:37:26 +0800

    no message

--- public/SwanlakeWeb/src/views/bscTokenStatistics/index.vue
+++ public/SwanlakeWeb/src/views/bscTokenStatistics/index.vue
@@ -46,7 +46,7 @@
                     <el-link v-else :underline="false" style="text-decoration:underline;" :href="'https://bscscan.com/token/' + selectAddress" target="_blank">{{ strAddress() }}</el-link>
                     <i class="el-icon-document-copy" style="cursor: pointer" v-clipboard:copy="selectAddress" v-clipboard:success="copySuccess"></i>
                 </div>
-                <div class="left-text" v-if="Object.keys(dataList).length">起始地址数：{{ numberFormatFilter(dataList.holders.data[0]) }} 最终地址数：{{ numberFormatFilter(dataList.holders.data[dataList.holders.data.length - 1]) }} 增加地址数：{{ numberFormatFilter(dataList.holders.add_holders) }} 增加百分比：{{ toFixed(dataList.holders.add_percentage, 2) }}%</div>
+                <div class="left-text" v-if="Object.keys(dataList).length">起始地址数：{{ numberFormatFilter(dataList.holders.data[0]) }} 最终地址数：{{ numberFormatFilter(dataList.holders.data[dataList.holders.data.length - 1]) }} 增加地址数：{{ numberFormatFilter(dataList.holders.add_holders) }} 增加百分比：{{ toFixed(dataList.holders.add_percentage, 4) }}%</div>
                 <div v-if="activeName == 1 && Object.keys(dataList).length" class="threeBarChart" id="countAddress"></div>
                 <el-empty v-else description="没有数据"></el-empty>
             </el-tab-pane>
@@ -67,7 +67,7 @@
                     <el-link v-else :underline="false" style="text-decoration:underline;" :href="'https://bscscan.com/token/' + selectAddress" target="_blank">{{ strAddress() }}</el-link>
                     <i class="el-icon-document-copy" style="cursor: pointer" v-clipboard:copy="selectAddress" v-clipboard:success="copySuccess"></i>
                 </div>
-                <div class="left-text" v-if="Object.keys(destructionDataList).length">起始销毁数：{{ numberFormatFilter(destructionDataList.balances.data[0]) }} 最终销毁数：{{ numberFormatFilter(destructionDataList.balances.data[destructionDataList.balances.data.length - 1]) }} 增加销毁数：{{ numberFormatFilter(destructionDataList.balances.add_destroy) }} 增加百分比：{{ toFixed(destructionDataList.balances.add_percentage, 2) }}%</div>
+                <div class="left-text" v-if="Object.keys(destructionDataList).length">起始销毁数：{{ numberFormatFilter(destructionDataList.balances.data[0]) }} 最终销毁数：{{ numberFormatFilter(destructionDataList.balances.data[destructionDataList.balances.data.length - 1]) }} 增加销毁数：{{ numberFormatFilter(destructionDataList.balances.add_destroy) }} 增加百分比：{{ toFixed(destructionDataList.balances.add_percentage, 4) }}%</div>
                 <div v-if="currencyIndex == 2" style="position: absolute;right: 190px;font-size: 13px;line-height: 23px;">自动销毁 {{ numberFormatFilter(autoDestruction) }} BNB, 手续费实时销毁 {{ numberFormatFilter(bnbNewValues) }} BNB，总计销毁（{{numberFormatFilter(autoDestruction + bnbNewValues)}}）BNB</div>
                 <div v-if="activeName == 3 && Object.keys(destructionDataList).length" class="threeBarChart" id="countDestruction"></div>
                 <el-empty v-else description="没有数据"></el-empty>
