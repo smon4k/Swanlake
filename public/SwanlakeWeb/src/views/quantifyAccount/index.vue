9705298aad59ca4b3c2e13b19d168a2814e51832
smon4k <smon4k@gmail.com>
2023-05-07 00:39:28 +0800

    no message

--- public/SwanlakeWeb/src/views/quantifyAccount/index.vue
+++ public/SwanlakeWeb/src/views/quantifyAccount/index.vue
@@ -321,7 +321,11 @@
                 </el-tab-pane>
                  <el-tab-pane label="收益率列表" name="3" v-if="tabAccountId == 7">
                     <el-table :data="maxMinUplRateList" style="width: 100%;" height="">
-                        <el-table-column label="ID" type="index"></el-table-column>
+                        <el-table-column label="序号">
+                            <template slot-scope="scope">
+                                {{ scope.$index + 1 }} 
+                            </template>
+                        </el-table-column>
                         <el-table-column prop="" label="持仓方向" align="center" width="">
                             <template slot-scope="scope">
                                 <span v-if="scope.row.pos_side === 'long'">多头</span>
