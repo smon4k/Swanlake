7424c3df9931e3a6d77cec145bda6296cfdd5140
qinlh <15035574759@163.com>
2024-09-20 22:20:59 +0800

    no message

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 header('Content-Type:text/html;charset=utf-8'); 
 header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
-header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding, Authorization");
+header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding, Authorization, exchange-token");
 if(isset($_SERVER['REQUEST_METHOD'])) {
     if(strtoupper($_SERVER['REQUEST_METHOD'])== 'OPTIONS'){ // 遇到options直接返回
         exit;
