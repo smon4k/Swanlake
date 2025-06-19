cdba95317b82f8d7b40beed70df7a69263d288c0
v-linhuiqin <v-linhuiqin@microsoft.com>
2025-06-19 11:23:54 +0800

    1

--- pyapi/piggybank/strategies/base_strategy.py
+++ pyapi/piggybank/strategies/base_strategy.py
@@ -29,12 +29,12 @@ class BaseStrategy(ABC):
         btc_balance = sum(
             Decimal(asset.get('availBal', '0')) for asset in details if asset.get('ccy') == currency1
         )
-        # print("btc_balance", btc_balance, currency1)
+        print("valuation btc_balance", btc_balance, currency1)
 
         usdt_balance = sum(
             Decimal(asset.get('availBal', '0')) for asset in details if asset.get('ccy') == currency2
         )
-        # print("usdt_balance", usdt_balance, currency2)
+        print("valuation usdt_balance", usdt_balance, currency2)
 
         btc_price = Decimal(str(ticker['last']))
         btc_valuation = btc_balance * btc_price
