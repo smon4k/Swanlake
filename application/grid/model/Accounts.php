f5be8d43d97dda83a25e7c519ee1269fa37e6bdc
v-linhuiqin <v-linhuiqin@microsoft.com>
2025-07-04 18:47:40 +0800

    1

--- application/grid/model/Accounts.php
+++ application/grid/model/Accounts.php
@@ -65,6 +65,7 @@ class Accounts extends Base
                 self::insert($insertData);
                 $insertId = self::getLastInsID();
                 if($insertId) {
+                    $insertData['id'] = $insertId;
                     $balanceList = QuantifyAccount::getOkxTradePairBalance($insertData);
                     if($balanceList && count((array)$balanceList) > 0) {
                         QuantifyAccount::calcQuantifyAccountData($insertId, 1, $balanceList['usdtBalance'], '第一笔入金');
