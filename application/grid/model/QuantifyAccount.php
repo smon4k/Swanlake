25f75690b60d856092a5727acb409bd1b5da5646
qinlh <15035574759@163.com>
2025-09-08 11:09:25 +0800

    no message

--- application/grid/model/QuantifyAccount.php
+++ application/grid/model/QuantifyAccount.php
@@ -1615,6 +1615,9 @@ class QuantifyAccount extends Base
     public static function getTransferHistory($accountInfo) {
         $url = Config('okx_uri') . "/api/okex/get_transfer_history";
         $balanceDetails = self::getOkxRequesInfo($accountInfo, $url, true);
+        if($accountInfo['financ_state'] == 1) {
+            return false;
+        }
         if ($balanceDetails && count($balanceDetails) > 0) {
             foreach ($balanceDetails as $transfer) {
                 $amount = abs($transfer['balChg']);
